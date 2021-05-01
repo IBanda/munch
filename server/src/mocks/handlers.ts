56eca119de51e8169712ea29151d9a156ae9fcff
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-01 20:21:38 +0800

    add rest handlers

--- server/src/mocks/handlers.ts
+++ server/src/mocks/handlers.ts
@@ -1,5 +1,24 @@
 import { rest } from 'msw';
 
+const places = [
+  {
+    name: 'Court Square Diner',
+    place_id: 'ChIJN6wFwihZwokRMtxwzGii9PI',
+    formatted_address: null,
+    opening_hours: {
+      open_now: true,
+    },
+  },
+  {
+    name: 'Bellwether',
+    place_id: 'ChIJ-SBdDCRZwokRu0x9Neuuoow',
+    formatted_address: null,
+    opening_hours: {
+      open_now: false,
+    },
+  },
+];
+
 export const handlers = [
   rest.get(
     'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
@@ -7,7 +26,20 @@ export const handlers = [
       return res(
         ctx.status(200),
         ctx.json({
-          results: [],
+          results: places,
+        })
+      );
+    }
+  ),
+  rest.get(
+    'https://maps.googleapis.com/maps/api/place/details/json',
+    (req, res, ctx) => {
+      const id = req.url.searchParams.get('place_id');
+      const place = places.find((place) => place.place_id === id);
+      return res(
+        ctx.status(200),
+        ctx.json({
+          result: place,
         })
       );
     }
