5bbb4bc561d1473c69a56a2db57f7c2786c17620
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-01 20:20:07 +0800

    use env db URI

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,16 +1,11 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI =
-  process.env.NODE_ENV === 'test'
-    ? 'mongodb://localhost:27017/munchTestDB'
-    : process.env.MONGO_URI;
-
 mongoose
-  .connect(MONGO_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
   })
   .catch(() => {
     process.exit(1);
