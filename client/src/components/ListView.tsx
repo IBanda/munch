4263317434d4520a1565cf4ea5b74161e5a9511e
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-03 06:56:27 +0800

    use context

--- client/src/components/ListView.tsx
+++ client/src/components/ListView.tsx
@@ -1,14 +1,20 @@
 import { ListView } from '@progress/kendo-react-listview';
 import { Place } from 'lib/interface';
 import PlaceCard from './Card';
+import { useDispatch } from './Context';
 
 interface Props {
   data: { places: Place[] };
 }
 
 const RenderItem = (props: any) => {
+  const setId = useDispatch();
   return (
-    <div className="mb-2">
+    <div
+      className="mb-2"
+      onMouseOver={() => setId(props.dataItem.place_id)}
+      onMouseOut={() => setId('')}
+    >
       <PlaceCard place={props.dataItem} />
     </div>
   );
--- client/src/pages/Places.tsx
+++ client/src/pages/Places.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Layout from 'components/Layout';
 import useGeo from 'lib/useGeo';
 import { useLazyQuery, gql } from '@apollo/client';
 import PlaceListView from 'components/ListView';
+import PlaceMap from 'components/PlaceMap';
+import { AppDispatchProvider, AppStateProvider } from 'components/Context';
 
 const GET_RESTAURANTS = gql`
   query GetRestaurants($coordinates: RestaurantInput) {
@@ -15,13 +17,21 @@ const GET_RESTAURANTS = gql`
       photos {
         photo_reference
       }
+      geometry {
+        location {
+          lat
+          lng
+        }
+      }
       vicinity
       types
     }
   }
 `;
+
 export default function Places() {
   const coords = useGeo();
+  const [id, setId] = useState('');
   const [getPlaces, { data, error }] = useLazyQuery(GET_RESTAURANTS);
   useEffect(() => {
     if (coords.lat || coords.lng) {
@@ -32,16 +42,22 @@ export default function Places() {
       });
     }
   }, [coords.lat, coords.lng, getPlaces]);
-
   if (!data && !error) return <p>Loading ...</p>;
   if (error) return <p>Error</p>;
+
   return (
     <Layout className="p-0" fluid>
       <div className="row no-gutters">
-        <div className="col-lg-6">
-          <PlaceListView data={data} />
+        <div className="col-lg-5 vh-100 overflow-auto">
+          <AppDispatchProvider setId={setId}>
+            <PlaceListView data={data} />
+          </AppDispatchProvider>
         </div>
-        <div className="col-lg-6"></div>
+        <div className="col-lg-7 vh-100">
+          <AppStateProvider id={id}>
+            <PlaceMap data={data} />
+          </AppStateProvider>
+        </div>
       </div>
     </Layout>
   );
