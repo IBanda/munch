131a36dd375d718edc62aa08576f1b2c0f83988b
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-02 21:29:23 +0800

    export fn

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,15 +1,29 @@
 import mongoose from 'mongoose';
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .catch(() => {
-    process.exit(1);
-  });
+const MONGO_URI =
+  process.env.NODE_ENV === 'test'
+    ? 'mongodb://localhost:27017/munchTestDB'
+    : process.env.MONGO_URI;
+
+export default function db() {
+  return {
+    connect() {
+      mongoose
+        .connect(MONGO_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+        })
+        .catch(() => {
+          process.exit(1);
+        });
+    },
+    disconnect() {
+      mongoose.disconnect();
+    },
+  };
+}
 
 mongoose.connection.on('error', (err) => {
   console.log(err);
