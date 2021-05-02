81f00dbc8363f0ef9997ca1bd6ff6a7ba50f80e6
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-03 06:49:49 +0800

    add google map

---
+++ client/src/components/PlaceMap.tsx
@@ -0,0 +1,35 @@
+import GoogleMapReact from 'google-map-react';
+import useGeo from 'lib/useGeo';
+import { Place } from 'lib/interface';
+import PlaceMarker from './PlaceMarker';
+
+interface Props {
+  data: { places: Place[] };
+}
+
+export default function PlaceMap({ data: { places } }: Props) {
+  const center = useGeo();
+  return (
+    <div className="h-100 w-100">
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: String(process.env.REACT_APP_GOOGLE_MAP_API_KEY),
+        }}
+        zoom={12}
+        center={{ lat: Number(center.lat), lng: Number(center.lng) }}
+        // center={{ lat: 51.53453534509865, lng: -0.118092 }}
+      >
+        {places.map((place) => (
+          <PlaceMarker
+            key={place?.place_id}
+            placeId={place?.place_id}
+            name={place?.name}
+            lat={place?.geometry?.location.lat}
+            lng={place?.geometry?.location.lng}
+            open={place?.opening_hours?.open_now}
+          />
+        ))}
+      </GoogleMapReact>
+    </div>
+  );
+}
