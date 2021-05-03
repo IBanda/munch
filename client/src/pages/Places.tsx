a49225edfd268747a54339397d5e45df2de146c8
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-03 21:00:06 +0800

    memo context values

--- client/src/pages/Places.tsx
+++ client/src/pages/Places.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import Layout from 'components/Layout';
 import useGeo from 'lib/useGeo';
 import { useLazyQuery, gql } from '@apollo/client';
 import PlaceListView from 'components/ListView';
 import PlaceMap from 'components/PlaceMap';
+import PlaceDetails from 'components/PlaceDetails';
 import { AppDispatchProvider, AppStateProvider } from 'components/Context';
 
 const GET_RESTAURANTS = gql`
@@ -32,7 +33,24 @@ const GET_RESTAURANTS = gql`
 export default function Places() {
   const coords = useGeo();
   const [id, setId] = useState('');
+  const [{ id: placeId, open }, setWindow] = useState({ open: false, id: '' });
   const [getPlaces, { data, error }] = useLazyQuery(GET_RESTAURANTS);
+
+  const dispatchContext = useMemo(
+    () => ({
+      setId,
+      setWindow,
+    }),
+    []
+  );
+
+  const appStateContext = useMemo(
+    () => ({
+      id,
+      setWindow,
+    }),
+    [id]
+  );
   useEffect(() => {
     if (coords.lat || coords.lng) {
       getPlaces({
@@ -44,17 +62,21 @@ export default function Places() {
   }, [coords.lat, coords.lng, getPlaces]);
   if (!data && !error) return <p>Loading ...</p>;
   if (error) return <p>Error</p>;
-
   return (
     <Layout className="p-0" fluid>
       <div className="row no-gutters">
-        <div className="col-lg-5 vh-100 overflow-auto">
-          <AppDispatchProvider setId={setId}>
+        <div
+          className={`col-lg-4 vh-100 ${
+            open ? 'overflow-hidden' : 'overflow-auto'
+          }`}
+        >
+          <AppDispatchProvider context={dispatchContext}>
             <PlaceListView data={data} />
           </AppDispatchProvider>
+          {open ? <PlaceDetails id={placeId} setWindow={setWindow} /> : null}
         </div>
-        <div className="col-lg-7 vh-100">
-          <AppStateProvider id={id}>
+        <div className="col-lg-8 vh-100">
+          <AppStateProvider context={appStateContext}>
             <PlaceMap data={data} />
           </AppStateProvider>
         </div>
