58f747853dbcc66542e2a226bc1e3b94e613265a
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-07 05:41:22 +0800

    add rating field

--- server/src/models/review.ts
+++ server/src/models/review.ts
@@ -4,12 +4,14 @@ interface Review extends Document {
   review: string;
   userId: string;
   placeId: string;
+  rating: number;
 }
 
 const ReviewSchema: Schema = new Schema({
   review: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, ref: 'user' },
   placeId: { type: String, required: true },
+  rating: Number,
   created_on: { type: String, default: Date.now() },
 });
 
