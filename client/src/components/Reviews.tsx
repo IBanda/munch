06f93660744c9eba6ef98d6a708571eafdd6235f
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-09 00:36:44 +0800

    add ratings

--- client/src/components/Reviews.tsx
+++ client/src/components/Reviews.tsx
@@ -3,6 +3,7 @@ import { useQuery, gql } from '@apollo/client';
 import ReviewEditor from './ReviewEditor';
 import ReviewList from './ReviewList';
 import { Review } from 'lib/interface';
+import Ratings from './Ratings';
 
 const GET_REVIEWS = gql`
   query GetReviews($placeId: ID!, $offset: Int = 0) {
@@ -63,6 +64,7 @@ export default function Reviews({ placeId }: Props) {
       variables: { placeId },
       updateQuery: (prev, { subscriptionData }) => {
         if (!subscriptionData.data) return prev;
+
         return {
           reviews: {
             reviews: [subscriptionData.data.review, ...prev.reviews.reviews],
@@ -80,17 +82,14 @@ export default function Reviews({ placeId }: Props) {
   if (loading) return <p>Loading...</p>;
 
   if (error) return <p>Error</p>;
-
-  console.log(data);
-  console.log(hasMore);
-
   return (
     <div className="m__details-reviews">
       <h2 className="font-weight-bold my-4 m__details-reviews-title">
         Customer Reviews
       </h2>
+      <Ratings placeId={placeId} />
       <ReviewList fetchMore={fetchMore} hasMore={hasMore} reviews={reviews} />
-      <div className="mt-4">
+      <div>
         <ReviewEditor placeId={placeId} />
       </div>
     </div>
