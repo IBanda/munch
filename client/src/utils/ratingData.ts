a95943041ff1745cede5a985090535ad7b2f6133
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-10 02:50:39 +0800

    rating data helper

---
+++ client/src/utils/ratingData.ts
@@ -0,0 +1,20 @@
+export default function ratingData(data: number[]) {
+  const ratingValue = [5, 4, 3, 2, 1];
+  const overallTotal = data.reduce((acc: number, cur: number, idx: number) => {
+    return acc + cur * ratingValue[idx];
+  }, 0);
+
+  const totalNumofRatings = data.reduce(
+    (acc: number, cur: number) => acc + cur,
+    0
+  );
+
+  const averageRating = Number((overallTotal / totalNumofRatings).toFixed(1));
+
+  return {
+    overallTotal,
+    totalNumofRatings,
+    averageRating,
+    ratingValue,
+  };
+}
