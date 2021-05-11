5982b549bbee7e89e3c3da9b5697985d3b689f68
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-12 01:17:28 +0800

    app loader

---
+++ client/src/components/AppLoader.tsx
@@ -0,0 +1,23 @@
+import {
+  Loader,
+  LoaderThemeColor,
+  LoaderType,
+} from '@progress/kendo-react-indicators';
+
+interface Props {
+  type?: LoaderType;
+  theme?: LoaderThemeColor;
+  wrapperClassName?: string;
+}
+
+export default function AppLoader({
+  type = 'converging-spinner',
+  theme = 'primary',
+  wrapperClassName,
+}: Props) {
+  return (
+    <div className={`d-flex justify-content-center ${wrapperClassName}`}>
+      <Loader type={type} themeColor={theme} />
+    </div>
+  );
+}
