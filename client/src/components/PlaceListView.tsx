df937cd2756b7d9586ee27cb9adad5fd2811e0b7
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-12 01:43:35 +0800

    use button instead

--- client/src/components/PlaceListView.tsx
+++ client/src/components/PlaceListView.tsx
@@ -1,29 +1,35 @@
 import { ListView } from '@progress/kendo-react-listview';
 import { Place } from 'lib/interface';
 import PlaceCard from './Card';
+import CardLoader from './CardLoader';
 import { useDispatch } from './Context';
 
 interface Props {
   data: Place[];
+  loading: boolean;
 }
 
 const RenderItem = (props: any) => {
   const { setId, setWindow } = useDispatch();
   const id = props.dataItem.place_id;
   return (
-    <div
-      className="mb-2"
+    <button
+      className="mb-2 w-100 bg-transparent border-0 text-left m__outline-none"
       onClick={() => setWindow({ open: true, id })}
       onMouseOver={() => setId(id)}
       onMouseOut={() => setId('')}
     >
       <PlaceCard place={props.dataItem} />
-    </div>
+    </button>
   );
 };
 
-export default function PlaceListView({ data }: Props) {
+export default function PlaceListView({ data, loading }: Props) {
   return (
-    <ListView data={data} item={RenderItem} className="m__places-listview" />
+    <ListView
+      data={data}
+      item={!loading ? RenderItem : CardLoader}
+      className="m__places-listview"
+    />
   );
 }
