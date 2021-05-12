031baafce257108d34c84c262d51562c1869d7f1
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-12 21:46:00 +0800

    add remove button

--- client/src/components/UploadListItem.tsx
+++ client/src/components/UploadListItem.tsx
@@ -2,7 +2,11 @@ import {
   UploadFileInfo,
   UploadListItemProps,
 } from '@progress/kendo-react-upload';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
+import { UploadState } from './Context';
+import { xCircleIcon } from '@progress/kendo-svg-icons';
+import { SvgIcon } from '@progress/kendo-react-common';
+import { Button } from '@progress/kendo-react-buttons';
 
 export default function UploadListItem({ files }: UploadListItemProps) {
   return (
@@ -19,10 +23,24 @@ interface PreviewImageProps {
 }
 function PreviewImage({ file }: PreviewImageProps) {
   const url = URL.createObjectURL(file.getRawFile?.());
+  const removeFile = useContext(UploadState);
+
   useEffect(() => {
     return () => {
       URL.revokeObjectURL(url);
     };
   }, [url]);
-  return <img className="m__upload-preview" src={url} alt={file.name} />;
+
+  return (
+    <div className="position-relative h-100 w-100">
+      <img className="m__upload-preview" src={url} alt={file.name} />
+      <Button
+        style={{ top: 0, right: 0 }}
+        onClick={() => removeFile?.(file.uid)}
+        className="position-absolute shadow-none px-2 bg-transparent m__outline-none border-0 z-index-2"
+      >
+        <SvgIcon icon={xCircleIcon} themeColor="error" />
+      </Button>
+    </div>
+  );
 }
