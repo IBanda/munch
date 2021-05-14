55e9fe7a2fbf22fbfde513f48afc3e1864df8510
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-14 19:57:12 +0800

    add sign in button

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,12 @@ export default function Header() {
   return (
     <header className="d-flex justify-content-between border p-2 align-items-center">
       <img className="logo" src="logo.jpg" alt="logo" />
-      <Button className="bg-success text-white py-1 btn-sm">Sign In</Button>
+      <div>
+        <Button className="bg-success text-white  btn-sm">Sign up</Button>
+        <Button look="outline" className=" ml-1  btn-sm">
+          Sign in
+        </Button>
+      </div>
     </header>
   );
 }
