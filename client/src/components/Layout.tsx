732b434efcec42202bd7277df8680ce83d70f399
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-17 04:02:09 +0800

    add authFormControl context

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { AuthFormControl } from './Context';
 import Header from './Header';
 
 interface Props {
@@ -7,10 +9,15 @@ interface Props {
 }
 
 export default function Layout({ children, fluid, className }: Props) {
+  const [modal, setModal] = useState<'hidden' | 'visible'>('hidden');
+  const [form, setForm] = useState<'signup' | 'login'>('login');
+
   return (
     <div className={`container${fluid ? '-fluid' : ''} ${className}`}>
-      <Header />
-      {children}
+      <AuthFormControl.Provider value={{ setModal, setForm, modal, form }}>
+        <Header />
+        {children}
+      </AuthFormControl.Provider>
     </div>
   );
 }
