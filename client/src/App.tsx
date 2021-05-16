c50e2e67f8e666e71368b729b0fee438deeaa4a4
Ian <38500123+IBanda@users.noreply.github.com>
2021-05-17 05:49:43 +0800

    remove unused variable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import Home from 'pages/Home';
-import { lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import Places from './pages/Places';
